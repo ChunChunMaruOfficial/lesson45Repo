69c12d25a067bcaad5d84f44ec3bc76cde7900aa
ChunChunMaru <93242480+ChunChunMaruOfficial@users.noreply.github.com>
2023-02-27 18:20:39 +0300

    Update Util.cpp

--- lesson45/Util.cpp
+++ lesson45/Util.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void rand_init(int* array, int size) {
 	for (int i = 0; i < size; i++)
 	{
-		array[i] = rand() % 100 - 50;
+		//array[i] = rand() % 100 - 50;
 	}
 }
 
@@ -14,7 +14,7 @@ string show(int* array, int size) {
 	string msg = "";
 	for (int i = 0; i < size; i++)
 	{
-		msg += to_string(array[i]) + " ";
+		//msg += to_string(array[i]) + " ";
 	}
 	return msg;
 }
