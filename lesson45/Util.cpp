632b64e2268bf738285247932404cabe7dd54ecf
ChunChunMaruOfficial <c4erkaoui@gmail.com>
2023-02-27 18:20:12 +0300

    1

---
+++ lesson45/Util.cpp
@@ -0,0 +1,34 @@
+#include <iostream> 
+#include <string> 
+
+using namespace std;
+
+void rand_init(int* array, int size) {
+	for (int i = 0; i < size; i++)
+	{
+		array[i] = rand() % 100 - 50;
+	}
+}
+
+string show(int* array, int size) {
+	string msg = "";
+	for (int i = 0; i < size; i++)
+	{
+		msg += to_string(array[i]) + " ";
+	}
+	return msg;
+}
+
+
+
+int main() {
+	srand(time(NULL));
+	int size = rand() % 20;
+
+	int* array = new int[size];
+
+	rand_init(array, size);
+	cout << show(array, size) << endl;
+
+	return 0;
+}
